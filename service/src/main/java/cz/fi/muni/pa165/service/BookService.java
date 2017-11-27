82a5e1ea13baaf1079ee9ee5aa25152cc50f8c10
mcada <mcada@redhat.com>
2017-11-27 18:30:32 +0100

    Adding business service method getAllBooksLoanedAfterDate

--- service/src/main/java/cz/fi/muni/pa165/service/BookService.java
+++ service/src/main/java/cz/fi/muni/pa165/service/BookService.java
@@ -1,11 +1,16 @@
 package cz.fi.muni.pa165.service;
 
 import cz.fi.muni.pa165.library.persistance.entity.Book;
+import cz.fi.muni.pa165.library.persistance.exceptions.DataAccessException;
 import cz.fi.muni.pa165.service.base.CrudService;
+
+import java.util.Date;
+import java.util.Set;
 
 /**
  *
  * @author Martin Palenik
  */
 public interface BookService extends CrudService<Book> {
+    Set<Book> getAllBooksLoanedAfterDate(Date date) throws DataAccessException;
 }
--- service/src/main/java/cz/fi/muni/pa165/service/BookServiceImpl.java
+++ service/src/main/java/cz/fi/muni/pa165/service/BookServiceImpl.java
@@ -1,11 +1,19 @@
 package cz.fi.muni.pa165.service;
 
 import cz.fi.muni.pa165.library.persistance.dao.BookDao;
+import cz.fi.muni.pa165.library.persistance.dao.LoanDao;
 import cz.fi.muni.pa165.library.persistance.dao.base.CrudDao;
 import cz.fi.muni.pa165.library.persistance.entity.Book;
+import cz.fi.muni.pa165.library.persistance.entity.Loan;
+import cz.fi.muni.pa165.library.persistance.entity.LoanItem;
+import cz.fi.muni.pa165.library.persistance.exceptions.DataAccessException;
 import cz.fi.muni.pa165.service.base.CrudServiceImpl;
 import javax.inject.Inject;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Implementation for Book service.
@@ -15,10 +23,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class BookServiceImpl extends CrudServiceImpl<Book> implements BookService {
 
-    private BookDao bookDao;
+    @Inject
+    private LoanDao loanDao;
     
     @Inject
     public BookServiceImpl(CrudDao<Book> crudDao) {
         super(crudDao);
+    }
+
+    @Override
+    public Set<Book> getAllBooksLoanedAfterDate(Date date) throws DataAccessException {
+        Set<Book> loanedBooks = new HashSet<>();
+
+        //maybe we can support this search via DAO and db query so it is faster
+        for(Loan loan : loanDao.findAll()) {
+            if(loan.getLoanCreated().after(date)) {
+                for (LoanItem item : loan.getLoanItems()) {
+                    loanedBooks.add(item.getBook());
+                }
+            }
+        }
+        return loanedBooks;
     }
 }
